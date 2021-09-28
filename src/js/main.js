19b1862f1d2a7bdd3afc2b6346562bddd801d714
Leizzz69 <leif.eggenfellner@gmail.com>
2021-09-28 13:55:38 +0200

    managed to draw array

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,31 @@
+"use strict";
+
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 500
-canvas.height = 500
+canvas.width = 1000;
+canvas.height = 500;
 
 const arrayToSort = Array.from({length: 50}, () => Math.floor(Math.random() * 100));
 
 const draw = () => {
+    const borderWidth = 2;
+    const offset = 2 * borderWidth;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
     ctx.beginPath();
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#FFF"
+    for (let i = 0; i < arrayToSort.length; i++) {
+        ctx.fillRect((canvas.width / arrayToSort.length) * i, canvas.height - borderWidth, canvas.width / arrayToSort.length, -5 * arrayToSort[i]);
+        ctx.rect((canvas.width / arrayToSort.length) * i, canvas.height - borderWidth, canvas.width / arrayToSort.length, -5 * arrayToSort[i]);
+    }
     ctx.closePath();
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 2;
+    ctx.stroke();
 }
 
 draw();
