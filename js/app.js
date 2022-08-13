075cdcdae80fc7c54918ce00b56c3ba336ded72f
leifeggenfellner <eggenfellner@protonmail.com>
2022-08-13 22:48:47 +0100

    add code for new button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-import { generateArray, drawArray, bubbleSort } from './functions.js';
+import Sorter from './Sorter.js';
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-const canvasWidth = 2560;
-const canvasHeight = 1200;
+const canvasWidth = 1920;
+const canvasHeight = 1000;
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
@@ -12,16 +12,24 @@ canvas.height = canvasHeight;
 const slider = document.getElementById('slider');
 const output = document.getElementsByTagName('output')[0];
 
+const generateNewArrayButton = document.getElementById('generateNewArray');
 const startSortingButton = document.getElementById('startSorting');
 const selectedAlgorithm = document.getElementById('algorithmSelect');
 
-let values = generateArray(slider.value, canvasHeight);
-drawArray(values, ctx, canvasWidth, canvasHeight);
+const sorter = new Sorter(ctx, canvasWidth, canvasHeight);
+sorter.generateArray(slider.value);
+sorter.draw();
+
+generateNewArrayButton.onclick = () => {
+  sorter.generateArray(slider.value);
+  sorter.draw();
+};
 
 startSortingButton.onclick = () => {
   switch (parseInt(selectedAlgorithm.value)) {
     case 0:
-      bubbleSort(values, ctx, canvasWidth, canvasHeight);
+      //   sorter.bubbleSort(ctx, canvasWidth, canvasHeight);
+      sorter.bubbleSort();
       break;
     case 1:
       console.log('Case 1');
@@ -31,6 +39,6 @@ startSortingButton.onclick = () => {
 
 slider.oninput = () => {
   output.value = slider.value;
-  values = generateArray(slider.value, canvasHeight);
-  drawArray(values, ctx, canvasWidth, canvasHeight);
+  sorter.generateArray(slider.value);
+  sorter.draw();
 };
