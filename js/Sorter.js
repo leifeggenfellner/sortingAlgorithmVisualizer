3f68b84e2942a5d872c3f75a7a26207ae5e80d97
leifeggenfellner <eggenfellner@protonmail.com>
2022-08-15 12:23:10 +0100

    fixed drawin

--- js/Sorter.js
+++ js/Sorter.js
@@ -3,56 +3,67 @@ export default class Sorter {
   #ctx;
   #canvasWidth;
   #canvasHeight;
+  #rectWidth;
+  #swapped;
+  #intervalID;
 
-  constructor(ctx, canvasWidth, canvasHeight) {
-    this.#arr = [];
+  constructor(arr, ctx, canvasWidth, canvasHeight, rectWidth) {
+    this.#arr = arr;
     this.#ctx = ctx;
     this.#canvasWidth = canvasWidth;
     this.#canvasHeight = canvasHeight;
+    this.#rectWidth = rectWidth;
+    this.#swapped = true;
+    this.#intervalID;
   }
 
-  generateArray(length) {
-    this.#arr = [];
-
-    for (let i = 0; i < length; i++) {
-      this.#arr[i] = Math.ceil(Math.random() * (this.#canvasHeight - 20) + 5);
-    }
-  }
-
-  bubbleSort() {
-    let length = this.#arr.length;
-
-    while (length >= 1) {
-      let newLength = 0;
-      for (let i = 1; i < length; i++) {
-        if (this.#arr[i - 1] > this.#arr[i]) {
-          [this.#arr[i - 1], this.#arr[i]] = [this.#arr[i], this.#arr[i - 1]];
-          newLength = i;
-        }
-      }
-      length = newLength;
-    }
-    this.draw();
-  }
-
-  draw() {
-    const rectWidth = this.#canvasWidth / this.#arr.length;
+  #displayArray = () => {
+    this.#ctx.lineWidth = 1;
 
     this.#ctx.clearRect(0, 0, this.#canvasWidth, this.#canvasHeight);
-    this.#ctx.lineWidth = 1;
-    this.#ctx.strokeStyle = '#000000';
-
     this.#ctx.beginPath();
     for (let i = 0; i < this.#arr.length; i++) {
-      this.#ctx.rect(
-        i * rectWidth,
-        this.#canvasHeight - this.#arr[i],
-        rectWidth,
-        this.#arr[i]
+      this.#ctx.moveTo(i * this.#rectWidth + 1, this.#canvasHeight);
+      this.#ctx.lineTo(
+        i * this.#rectWidth + 1,
+        this.#canvasHeight - this.#arr[i]
       );
-
-      this.#ctx.stroke();
+      this.#ctx.closePath();
     }
-    this.#ctx.closePath();
-  }
+
+    this.#ctx.stroke();
+  };
+
+  #sortArray = () => {
+    this.#swapped = false;
+
+    for (let i = 1; i < this.#arr.length; i++) {
+      if (this.#arr[i - 1] > this.#arr[i]) {
+        [this.#arr[i - 1], this.#arr[i]] = [this.#arr[i], this.#arr[i - 1]];
+        this.#swapped = true;
+      }
+    }
+  };
+
+  #bubbleSort = () => {
+    this.#sortArray();
+    this.#displayArray();
+  };
+
+  bubbleSortLoop = () => {
+    this.swapped === true
+      ? (this.#intervalID = setInterval(this.#bubbleSort, 100))
+      : clearInterval(this.#intervalID);
+    setInterval(this.#bubbleSort, 100);
+  };
+
+  generateArray = (n, v1, v2) => {
+    this.#arr.length = 0;
+
+    for (let i = 0; i < n; i++) {
+      arr.push(Math.round(Math.random() * (v2 - v1) + v1));
+    }
+
+    return arr;
+  };
 }
