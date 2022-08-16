93cec93cbef33841f72dcb935c4ab05505a2c14e
leifeggenfellner <eggenfellner@protonmail.com>
2022-08-16 14:22:12 +0200

    create new function and change variables

--- js/Sorter.js
+++ js/Sorter.js
@@ -1,10 +1,17 @@
+/**
+ *
+ *
+ * @export
+ * @class Sorter
+ */
+
 export default class Sorter {
   #arr;
   #ctx;
   #canvasWidth;
   #canvasHeight;
   #rectWidth;
-  #swapped;
+  swapped;
   #intervalID;
 
   constructor(arr, ctx, canvasWidth, canvasHeight, rectWidth) {
@@ -13,57 +20,69 @@ export default class Sorter {
     this.#canvasWidth = canvasWidth;
     this.#canvasHeight = canvasHeight;
     this.#rectWidth = rectWidth;
-    this.#swapped = true;
-    this.#intervalID;
+    this.swapped = true;
+    this.#intervalID = 0;
   }
 
-  #displayArray = () => {
-    this.#ctx.lineWidth = 1;
+  displayArray = () => {
+    this.#rectWidth = this.#canvasWidth / this.#arr.length;
 
     this.#ctx.clearRect(0, 0, this.#canvasWidth, this.#canvasHeight);
+
+    this.#ctx.fillStyle = '#B48EAD';
+    this.#ctx.lineWidth = this.#arr.length >= 65 ? 1 : 2;
+
     this.#ctx.beginPath();
     for (let i = 0; i < this.#arr.length; i++) {
-      this.#ctx.moveTo(i * this.#rectWidth + 1, this.#canvasHeight);
-      this.#ctx.lineTo(
-        i * this.#rectWidth + 1,
-        this.#canvasHeight - this.#arr[i]
+      this.#ctx.rect(
+        i * this.#rectWidth,
+        this.#canvasHeight - this.#arr[i],
+        this.#rectWidth,
+        this.#arr[i]
       );
       this.#ctx.closePath();
     }
 
+    this.#ctx.fill();
     this.#ctx.stroke();
   };
 
   #sortArray = () => {
-    this.#swapped = false;
+    this.swapped = false;
 
     for (let i = 1; i < this.#arr.length; i++) {
       if (this.#arr[i - 1] > this.#arr[i]) {
         [this.#arr[i - 1], this.#arr[i]] = [this.#arr[i], this.#arr[i - 1]];
-        this.#swapped = true;
+        this.swapped = true;
       }
     }
   };
 
   #bubbleSort = () => {
     this.#sortArray();
-    this.#displayArray();
+    this.displayArray();
   };
 
-  bubbleSortLoop = () => {
+  #sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  bubbleSortLoop = async () => {
+    while (this.swapped) {
+      this.#bubbleSort();
+      await this.#sleep(100);
+    }
+
     this.swapped === true
       ? (this.#intervalID = setInterval(this.#bubbleSort, 100))
       : clearInterval(this.#intervalID);
-    setInterval(this.#bubbleSort, 100);
   };
 
-  generateArray = (n, v1, v2) => {
+  generateArray = (length) => {
     this.#arr.length = 0;
 
-    for (let i = 0; i < n; i++) {
-      arr.push(Math.round(Math.random() * (v2 - v1) + v1));
+    for (let i = 0; i < length; i++) {
+      this.#arr.push(Math.round(Math.random() * (this.#canvasHeight - 20) + 5));
     }
-
-    return arr;
   };
 }
