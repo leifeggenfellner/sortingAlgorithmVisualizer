b5a873c7c0397b533830fe0ff79383465ea2c17c
leifeggenfellner <eggenfellner@protonmail.com>
2022-08-16 14:23:00 +0200

    add feature to draw array sorting

--- js/app.js
+++ js/app.js
@@ -1,44 +1,59 @@
+import { generateArray } from './funcs.js';
 import Sorter from './Sorter.js';
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-const canvasWidth = 1920;
-const canvasHeight = 1000;
-
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+const canvasWidth = (canvas.width = canvas.offsetWidth);
+const canvasHeight = (canvas.height = canvas.offsetHeight);
 
 const slider = document.getElementById('slider');
 const output = document.getElementsByTagName('output')[0];
 
 const generateNewArrayButton = document.getElementById('generateNewArray');
 const startSortingButton = document.getElementById('startSorting');
-const selectedAlgorithm = document.getElementById('algorithmSelect');
+const selectedAlgorithm = document.getElementById('algorithms-choice');
 
-const sorter = new Sorter(ctx, canvasWidth, canvasHeight);
-sorter.generateArray(slider.value);
-sorter.draw();
+const arr = generateArray(slider.value, canvasHeight);
+const rectWidth = Math.floor(canvasWidth / slider.value);
+
+const sorter = new Sorter(arr, ctx, canvasWidth, canvasHeight, rectWidth);
+sorter.displayArray();
+
+let prevSortMethod = selectedAlgorithm.value || 'Bubble Sort';
 
 generateNewArrayButton.onclick = () => {
+  if (!sorter.swapped) {
+    sorter.swapped = true;
+  } else {
+    sorter.swapped = false;
+  }
+
   sorter.generateArray(slider.value);
-  sorter.draw();
+  sorter.displayArray();
 };
 
 startSortingButton.onclick = () => {
-  switch (parseInt(selectedAlgorithm.value)) {
-    case 0:
-      //   sorter.bubbleSort(ctx, canvasWidth, canvasHeight);
-      sorter.bubbleSort();
+  if (!sorter.swapped && selectedAlgorithm.value !== prevSortMethod) {
+    sorter.swapped = true;
+  }
+
+  switch (selectedAlgorithm.value || 'Bubble Sort') {
+    case 'Bubble Sort':
+      sorter.bubbleSortLoop();
       break;
-    case 1:
+    case 'Console Log':
       console.log('Case 1');
       break;
   }
 };
 
 slider.oninput = () => {
+  if (sorter.swapped) {
+    sorter.swapped = false;
+  }
+
   output.value = slider.value;
   sorter.generateArray(slider.value);
-  sorter.draw();
+  sorter.displayArray();
 };
